037462c86b35d1a216676453ac9ae5c38b24ce29
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-03-25 23:48:00 +0530

    carousel in progress

--- javascript/featureCarousel.js
+++ javascript/featureCarousel.js
@@ -1,7 +1,34 @@
 let featureCard = document.querySelectorAll(".featureCard")
-base = 100/3
-console.log(featureCard)
+let leftBtn = document.querySelector(".featureLeftBtn")
+let rightBtn = document.querySelector(".featureRightBtn")
 
+
+base = 100/3
+counter = 0
+//setting the initial left value
 for(i = 0; i<featureCard.length; i++){
     featureCard[i].style.left = `${base*i}%`
 }
+
+
+function slideLeft(){
+    if(counter == featureCard.length-1) return
+    counter++
+    for(i = 0; i<featureCard.length; i++){
+        let val = Number.parseInt(featureCard[i].style.left)
+        val -= base
+        featureCard[i].style.left = `${val}%`
+    }
+}
+
+function slideRight(){
+    if(counter == 0) return
+    counter--
+    for(i = 0; i<featureCard.length; i++){
+        let val = Number.parseInt(featureCard[i].style.left)
+        val += base
+        featureCard[i].style.left = `${val}%`
+    }
+}
+
+
