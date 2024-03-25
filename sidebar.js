881ab1ff0ecc1534906938681dbfb653e322aef0
Srajan Saxena <srajan.saxena7@gmail.com>
2024-03-25 16:26:45 +0530

    branch in sync

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,26 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+//-------Nav Bar Toggle--------->>
+let flag = false;
+
+function toggleNav() {
+  let sideBar = document.getElementById("mySidebar")
+  if (!flag) sideBar.style.left = 0
+  else sideBar.style.left = '-20%'
+
+  flag = !flag;
+}
+
+//-------Header Toggle on scroll--------->>
+let prevScrollY = 0
+let head = document.querySelector(".header")
+
+window.addEventListener("scroll", () => {
+  let currScrollY = window.scrollY
+  if(currScrollY > prevScrollY){
+    head.style.top = '-10vh'
+  }
+  else{
+    head.style.top = '0'
   }
   
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  prevScrollY = currScrollY
+})
