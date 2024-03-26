fd62ca73a1689aeef96563bd40a8385f5fbd1a11
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-03-26 09:26:37 +0530

    feature section ready

--- javascript/featureCarousel.js
+++ javascript/featureCarousel.js
@@ -3,32 +3,58 @@ let leftBtn = document.querySelector(".featureLeftBtn")
 let rightBtn = document.querySelector(".featureRightBtn")
 
 
-base = 100/3
-counter = 0
+let base = 25
+let count = 0
+let n = featureCard.length
+console.log(n)
+console.log(typeof n)
+
 //setting the initial left value
-for(i = 0; i<featureCard.length; i++){
-    featureCard[i].style.left = `${base*i}%`
+for (i = 0; i < n; i++) {
+    featureCard[i].style.left = `${base * i}%`
 }
 
 
-function slideLeft(){
-    if(counter == featureCard.length-1) return
-    counter++
-    for(i = 0; i<featureCard.length; i++){
-        let val = Number.parseInt(featureCard[i].style.left)
-        val -= base
-        featureCard[i].style.left = `${val}%`
+function slideLeft() {
+    if (count == (n - 1) - 4) {
+        count = 0
+        for (i = 0; i < n; i++) {
+            featureCard[i].style.left = `${base * i}%`
+        }
+    }
+    else {
+        count++
+        for (i = 0; i < n; i++) {
+            let val = Number.parseInt(featureCard[i].style.left)
+            val -= base
+            featureCard[i].style.left = `${val}%`
+        }
     }
 }
 
-function slideRight(){
-    if(counter == 0) return
-    counter--
-    for(i = 0; i<featureCard.length; i++){
-        let val = Number.parseInt(featureCard[i].style.left)
-        val += base
-        featureCard[i].style.left = `${val}%`
+function slideRight() {
+    if (count == 0) {
+        count = (n - 1) - 4
+        let tempBase = 75
+        for (i = n-1; i >= 0; i--) {
+            featureCard[i].style.left = `${tempBase}%`
+            tempBase -= 25
+        }
+
+    }
+    else {
+        count--
+        for (i = 0; i < n; i++) {
+            let val = Number.parseInt(featureCard[i].style.left)
+            val += base
+            featureCard[i].style.left = `${val}%`
+        }
     }
 }
+
+//automating
+setInterval(()=>{
+    rightBtn.click()
+}, 3000)
 
 
